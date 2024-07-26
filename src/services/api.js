2f9006c5d143305b89d5715e46a17bbd9ce9be46
dennis <dennis.eckert0218@gmail.com>
2024-07-26 12:54:51 +0200

    change api base url

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000/api' });
+const API = axios.create({ baseURL: 'https://kodi-subscription-server.vercel.app/api' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
